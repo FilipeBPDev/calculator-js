e6e7e1b87db3b2bd07bbbdd4769162641bcbfce3
Filipe <devfilipebp@gmail.com>
2023-09-14 16:13:16 -0300

    :sparkles: Create function Calculate button

--- main.js
+++ main.js
@@ -4,69 +4,74 @@ import AreaApp from './src/objects/AppArea/index.js';
 //toda variável que começa com $ ela guarda umelemento da tela - boa prática.
 const $root = document.querySelector('#root');
 const $htmlAreaApp = AreaApp();
-//armazenar div area total
-//criar variaveis para armazenar as divs dos display sinais e display igual
-
-
 
 //forma segura de inserir HTML num alvo. (insertAdjacentHTML)
 $root.insertAdjacentHTML('beforeend', $htmlAreaApp);
 
 //operações responsáveis por exibir no display result o valor do botão clicado
-let result = document.querySelector('#display');
 const btnNum = document.querySelectorAll('.disp-btn');
 const cBtn = document.querySelector('.clear');
 const dBtn = document.querySelector('.del');
 const eqlBtn = document.querySelector('.equal');
-const operators = document.querySelectorAll('.operator-button')
-
+let result = document.querySelector('#display');
 let btnsClicked = '';
-let currentIn = '';
-let currentOperator = '';
 
 //function exibir valor dos botões
 const listenBtn = (event) => {
     const btnValue = event.target.textContent;
     result.value += btnValue
     btnsClicked += btnValue
-
 };
 
-
-
-
-const calculateBtn = (value) => {
+const calculateBtn = () => {
     //separa todos os valores que estão divididos por +-*/
     const nums = btnsClicked.split(/[+\-*\/]/);
-    const op = btnsClicked.split(/[0-9]+/).filter(Boolean);
-
+    const operators = btnsClicked.split(/[0-9.]+/).filter(Boolean);
     const numbers = nums.map((num) => {
         return Number(num);
-    });
-    console.log(numbers, op)
-    
+    }); 
 
+    //começa no primeiro valor digitado
+    let resultValue = numbers[0]; 
 
+    for(let i = 0; i < operators.length; i++) {
+        const op = operators[i];
+        const nextNum = numbers[i + 1];
 
-
+        //verifica o operador e executa o cálculo
+        switch (op) {
+        case '+':
+            resultValue += nextNum;
+            break;
+        case '-':
+            resultValue -= nextNum;
+            break;
+        case '*':
+            resultValue *= nextNum;
+            break;
+        case '/':
+            if (nextNum === 0) {
+                resultValue = '[ERROR] Não é possível efetuar divisão por 0'
+                return;
+            }
+            resultValue /= nextNum
+            break;
+        default:
+            break;
+        }  
+    }
+    result.value = resultValue.toString();
 }
-
 
 const clearBtn = () => {
     result.value = '';
-    displayValue = '';
-    currentIn = '';
-    currentOperator = '';
+    btnsClicked = '';
 };
 
 //function apagar o último valor clicado.
 const delBtn = () => {
-    result.value = result.value.slice(0, -2); //-2 pois apaga o último valor e o espaço entre eles.
+    result.value = result.value.slice(0, -1); //-2 pois apaga o último valor e o espaço entre eles.
 }
-
-
-
-
 
 //ouvinte dos btns de números
 btnNum.forEach((btn) => {
@@ -76,6 +81,5 @@ btnNum.forEach((btn) => {
 cBtn.addEventListener('click', clearBtn);
 //ouvinte botão del
 dBtn.addEventListener('click', delBtn);
-
 eqlBtn.addEventListener('click', calculateBtn);
 
